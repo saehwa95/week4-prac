ea45438d574af7d91e25790003949b613ff1fe52
Saehwa95 <tpghk1002@naver.com>
2022-12-03 20:57:37 +0900

    주특기 숙련 리덕스 완강

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
 import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { addNumber, minusNumber } from "./redux/modules/counter";
 
 const App = () => {
   const [number, setNumber] = useState(0);
+
+  const globalNumber = useSelector((state) => state.counter.number);
+
+  const dispatch = useDispatch();
+
   const onChangeHandler = (e) => {
     //이벤트 핸들러가 자동으로 문자열로 설정되어 있는 상태
     const { value } = e.target;
     // 숫자형 변환을 위해 + 붙여줌
     setNumber(+value);
   };
+
+  const onClickAddNumberHandler = () => {
+    dispatch(addNumber(number));
+  };
+
+  const onClickMinusNumberHandler = () => {
+    dispatch(minusNumber(number));
+  };
+
   console.log(number);
+
   return (
     <div>
+      {globalNumber}
       <input type="number" onChange={onChangeHandler} />
-      <button>더하기</button>
-      <button>빼기</button>
+      <button onClick={onClickAddNumberHandler}>더하기</button>
+      <button onClick={onClickMinusNumberHandler}>빼기</button>
     </div>
   );
 };
--- src/redux/modules/counter.js
+++ src/redux/modules/counter.js
@@ -1,23 +1,23 @@
-// src/modules/counter.js
+//액션
+const ADD_NUMBER = "ADD_NUMBER";
+const MINUS_NUMBER = "MINUS_NUMBER";
 
-// 추가된 코드 👇 - 액션 value를 상수들로 만들어 줍니다. 보통 이렇게 한곳에 모여있습니다.
-const PLUS_ONE = "PLUS_ONE";
-const MINUS_ONE = "MINUS_ONE";
-
-// 추가된 코드 👇 - Action Creator를 만들어 줍니다.
-export const plusOne = () => {
+// 액션 크리에이터
+export const addNumber = (payload) => {
   return {
-    type: PLUS_ONE,
+    type: ADD_NUMBER,
+    payload,
   };
 };
 
-export const minusOne = () => {
+export const minusNumber = (payload) => {
   return {
-    type: MINUS_ONE,
+    type: MINUS_NUMBER,
+    payload,
   };
 };
 
-// 초기 상태값
+// 초기값 세팅
 const initialState = {
   number: 0,
 };
@@ -25,14 +25,18 @@ const initialState = {
 // 리듀서
 const counter = (state = initialState, action) => {
   switch (action.type) {
-    case PLUS_ONE: // case에서도 문자열이 아닌, 위에서 선언한 상수를 넣어줍니다.
+    case ADD_NUMBER: {
       return {
-        number: state.number + 1,
+        number: state.number + action.payload,
       };
-    case MINUS_ONE: // case에서도 문자열이 아닌, 위에서 선언한 상수를 넣어줍니다.
+    }
+
+    case MINUS_NUMBER: {
       return {
-        number: state.number - 1,
+        number: state.number - action.payload,
       };
+    }
+
     default:
       return state;
   }
